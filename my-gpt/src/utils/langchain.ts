e95ad3b1a04eaa2363a7a772d90b8e734cc91a5e
guilhermeb <guilherme.barreto@retarus.pt>
2024-06-05 11:20:20 +0100

    feat: add exercise 5

--- my-gpt/src/utils/langchain.ts
+++ my-gpt/src/utils/langchain.ts
@@ -1,12 +1,21 @@
 import { OpenAI } from "@langchain/openai";
+import { PromptTemplate } from "@langchain/core/prompts";
 
 const llm = new OpenAI({ openAIApiKey: import.meta.env.VITE_OPENAI_KEY });
 
 export async function generateAnswer(question: string) {
+  const prompt = PromptTemplate.fromTemplate(
+    `Take the role of a personal travel assistant, and answer the following question in detail: {question}?`
+  );
+
   let answer = ''
 
+  const formattedPrompt = await prompt.format({
+    question,
+  });
+
   try {
-    answer = await llm.invoke(question);
+    answer = await llm.invoke(formattedPrompt);
   } catch (e) {
       return 'Something went wrong'
   }
